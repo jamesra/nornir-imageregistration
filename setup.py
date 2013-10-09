ce7e16abb3da87f4865a6780dc7138af08ba4f42
jamesra <james.r.anderson@utah.edu>
2013-10-09 17:12:42 -0600

    Fixed Type

--- setup.py
+++ setup.py
@@ -12,13 +12,7 @@ from setuptools import setup, find_packages
 
 if __name__ == '__main__':
     use_setuptools()
-
-    required_packages = ["nornir_pools",
-                         "nornir_shared",
-                        "numpy>=1.7.1"",
-                        "scipy>=0.12",
-                        "matplotlib"]
-
+    
     packages = ["nornir_imageregistration",
                 "nornir_imageregistration.io",
                 "nornir_imageregistration.geometry",
@@ -26,8 +20,9 @@ if __name__ == '__main__':
 
     install_requires = ["nornir_pools",
                         "nornir_shared",
-                        "numpy",
-                        "scipy"]
+                        "numpy>=1.7.1",
+                        "scipy>=0.12",
+                        "matplotlib"]
 
     dependency_links = ["git+http://github.com/jamesra/nornir-pools#egg=nornir_pools",
                         "git+http://github.com/jamesra/nornir-shared#egg=nornir_shared"]
@@ -42,5 +37,4 @@ if __name__ == '__main__':
           test_suite='test',
           install_requires=install_requires,
           test_requires=install_requires,
-          dependency_links=dependency_links,
-          requires=required_packages)
+          dependency_links=dependency_links )
