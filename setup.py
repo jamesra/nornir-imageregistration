29af3383487472e2eacc988b7116d223955054b8
James Anderson <james.r.andreson@utah.edu>
2013-10-14 10:47:25 -0700

    Added scripts to the nornir-imageregistration package in a more general way.  More consistent with the other build manager scripts.

--- setup.py
+++ setup.py
@@ -9,10 +9,12 @@ Created on Aug 30, 2013
 
 from ez_setup import use_setuptools
 from setuptools import setup, find_packages
+import os
+import glob
 
 if __name__ == '__main__':
     use_setuptools()
-    
+
     packages = ["nornir_imageregistration",
                 "nornir_imageregistration.io",
                 "nornir_imageregistration.geometry",
@@ -27,6 +29,8 @@ if __name__ == '__main__':
     dependency_links = ["git+http://github.com/jamesra/nornir-pools#egg=nornir_pools",
                         "git+http://github.com/jamesra/nornir-shared#egg=nornir_shared"]
 
+    scripts = glob.glob(os.path.join('scripts', '*.py'))
+
     setup(name='nornir_imageregistration',
           version='1.0',
           description="Contains the core image registration algorithms for aligning 2d images into larger mosaics and 3D volumes",
@@ -34,6 +38,7 @@ if __name__ == '__main__':
           author_email="James.R.Anderson@utah.edu",
           url="https://github.com/jamesra/nornir-imageregistration",
           packages=packages,
+          scripts=scripts,
           test_suite='test',
           install_requires=install_requires,
           test_requires=install_requires,
