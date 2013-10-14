597c40e969921c0765d3291ac71ba470c56ef837
James Anderson <james.r.andreson@utah.edu>
2013-10-14 10:47:50 -0700

    Adding forgotten scripts

---
+++ scripts/nornir-addtransforms.py
@@ -0,0 +1,100 @@
+'''
+Created on May 21, 2013
+
+@author: u0490822
+'''
+
+import argparse
+import os
+import IrTools.IO.stosfile as stosfile
+import Utils.Misc
+import logging
+import sys
+
+def __CreateArgParser(ExecArgs=None):
+
+
+
+    # conflict_handler = 'resolve' replaces old arguments with new if both use the same option flag
+    parser = argparse.ArgumentParser(description="Maps the control space of the warped transform to the control space of the fixed transform and saves the resulting transform as a new .stos file.")
+
+    parser.add_argument('-output', '-o',
+                        action='store',
+                        required=True,
+                        type=str,
+                        help='Output transform file path',
+                        dest='outputpath')
+
+    parser.add_argument('-fixed', '-f',
+                        action='store',
+                        required=True,
+                        type=str,
+                        default=None,
+                        help='Fixed transform path',
+                        dest='fixedpath'
+                        )
+
+    parser.add_argument('-warped', '-w',
+                        action='store',
+                        required=True,
+                        type=str,
+                        default=None,
+                        help='Warped transform path, ',
+                        dest='warpedpath'
+                        )
+
+    return parser;
+
+def ParseArgs(ExecArgs=None):
+    if ExecArgs is None:
+        ExecArgs = sys.argv;
+
+    parser = __CreateArgParser();
+
+    return parser.parse_known_args(args=ExecArgs)
+
+
+def OnUseError(message):
+    parser = __CreateArgParser()
+    parser.print_usage()
+
+    log = logging.getLogger('AddTransforms')
+    log.error(message)
+
+    sys.exit()
+
+def ValidateArgs(Args):
+    if not os.path.exists(Args.fixedpath):
+        OnUseError("Fixed stos file not found: " + Args.fixedpath)
+
+    if not os.path.exists(Args.warpedpath):
+        OnUseError("Warped stos file not found: " + Args.warpedpath)
+
+    if not os.path.exists(os.path.dirname(Args.outputpath)):
+        os.makedirs(os.path.dirname(Args.outputpath))
+
+
+def Execute(ExecArgs=None):
+
+    (Args, extra) = ParseArgs(ExecArgs)
+
+    ValidateArgs(Args)
+
+    MToVStos = stosfile.AddStosTransforms(Args.warpedpath, Args.fixedpath)
+    MToVStos.Save(Args.outputpath)
+
+    if os.path.exists(Args.outputpath):
+        print "Wrote: " + Args.outputpath
+    else:
+        print "Outputfile is missing, unknown error: " + Args.outputpath
+
+
+if __name__ == '__main__':
+
+    (args, extra) = ParseArgs();
+
+    Utils.Misc.SetupLogging(os.path.join(os.path.dirname(args.outputpath), "Logs"))
+
+    Execute();
+
+    pass
---
+++ scripts/nornir-slicetomosaic.py
@@ -0,0 +1,126 @@
+'''
+Created on May 21, 2013
+
+@author: u0490822
+'''
+
+import argparse
+import os
+import IrTools.assemble
+import Utils.Misc
+import logging
+import sys
+
+def __CreateArgParser(ExecArgs=None):
+
+
+
+    # conflict_handler = 'resolve' replaces old arguments with new if both use the same option flag
+    parser = argparse.ArgumentParser(description="Produce a registered image for the moving image in a .stos file");
+
+    parser.add_argument('-input', '-i',
+                        action='store',
+                        required=True,
+                        type=str,
+                        help='Input .stos file path',
+                        dest='inputpath');
+
+    parser.add_argument('-output', '-o',
+                        action='store',
+                        required=True,
+                        type=str,
+                        help='Output image file path',
+                        dest='outputpath');
+
+    parser.add_argument('-fixed', '-f',
+                        action='store',
+                        required=False,
+                        type=str,
+                        default=None,
+                        help='Fixed image path, overrides .stos file fixed image entry',
+                        dest='fixedimagepath'
+                        );
+
+    parser.add_argument('-warped', '-w',
+                        action='store',
+                        required=False,
+                        type=str,
+                        default=None,
+                        help='Warping image path, overrides .stos file fixed image entry',
+                        dest='warpedimagepath'
+                        );
+
+    parser.add_argument('-scale', '-s',
+                        action='store',
+                        required=False,
+                        type=float,
+                        default=1.0,
+                        help='The input images are a different size than the transform, scale the transform by the specified factor',
+                        dest='scalar'
+                        );
+
+    return parser;
+
+def ParseArgs(ExecArgs=None):
+    if ExecArgs is None:
+        ExecArgs = sys.argv;
+
+    parser = __CreateArgParser();
+
+    return parser.parse_known_args(args=ExecArgs)
+
+
+def OnUseError(message):
+    parser = __CreateArgParser()
+    parser.print_usage()
+
+    log = logging.getLogger('Ir-Stom')
+    log.error(message)
+
+    sys.exit()
+
+def ValidateArgs(Args):
+    if not os.path.exists(Args.inputpath):
+        OnUseError("Input stos file not found: " + Args.inputpath)
+
+    if not os.path.exists(os.path.dirname(Args.outputpath)):
+        os.makedirs(os.path.dirname(Args.outputpath))
+
+    if not Args.fixedimagepath is None:
+        if not os.path.exists(Args.fixedimagepath):
+            OnUseError("Fixed image file not found: " + Args.fixedimagepath)
+
+    if not Args.warpedimagepath is None:
+        if not os.path.exists(Args.warpedimagepath):
+            OnUseError("Warped image file not found: " + Args.warpedimagepath)
+
+    if not os.path.exists(Args.inputpath):
+        OnUseError("Input stos file not found: " + Args.inputpath)
+
+def Execute(ExecArgs=None):
+
+    (Args, extra) = ParseArgs(ExecArgs)
+
+    ValidateArgs(Args)
+
+    IrTools.assemble.TransformStos(transformFullpath=Args.inputpath,
+                                   OutputFilename=Args.outputpath,
+                                   fixedImageFilename=Args.fixedimagepath,
+                                   warpedImageFilename=Args.warpedimagepath,
+                                   scalar=Args.scalar)
+
+    if os.path.exists(Args.outputpath):
+        print "Wrote: " + Args.outputpath
+    else:
+        print "Outputfile is missing, unknown error: " + Args.outputpath
+
+
+if __name__ == '__main__':
+
+    (args, extra) = ParseArgs();
+
+    Utils.Misc.SetupLogging(os.path.join(os.path.dirname(args.outputpath), "Logs"))
+
+    Execute();
+
+    pass
