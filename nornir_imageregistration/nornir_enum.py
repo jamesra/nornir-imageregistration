10230123ecf6d6fb3c5ac536c592449caa21746c
James Anderson <james.r.andreson@utah.edu>
2014-04-17 15:52:55 -0700

    Re-add file for enums that was deleted during port

---
+++ nornir_imageregistration/nornir_enum.py
@@ -0,0 +1,11 @@
+'''
+Created on Jun 4, 2013
+
+@author: u0490822
+'''
+
+def enum(*sequential, **named):
+    '''Generates a dictionary of names to number values used as an enumeration.
+       Copied from a StackOverflow post'''
+    enums = dict(list(zip(sequential, list(range(len(sequential))))), **named)
+    return type('Enum', (), enums)
