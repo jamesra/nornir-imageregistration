7e7911f71b439f984a5b3b21890767ea4e05e350
jamesra <james.r.anderson@utah.edu>
2014-10-07 18:48:33 -0600

    Return None for MaskFullPath properties instead of exception if the mask values are unset

--- nornir_imageregistration/files/stosfile.py
+++ nornir_imageregistration/files/stosfile.py
@@ -97,6 +97,9 @@ class StosFile(object):
 
     @property
     def ControlMaskFullPath(self):
+        if self.ControlMaskPath is None or self.ControlMaskName is None:
+            return None
+        
         return os.path.join(self.ControlMaskPath, self.ControlMaskName)
 
     @ControlMaskFullPath.setter
@@ -109,6 +112,9 @@ class StosFile(object):
 
     @property
     def MappedMaskFullPath(self):
+        if self.MappedMaskPath is None or self.MappedMaskName is None:
+            return None
+        
         return os.path.join(self.MappedMaskPath, self.MappedMaskName)
 
     @MappedMaskFullPath.setter
