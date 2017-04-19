530f9588df5723fe11d05070a45b2d3badd89359
James Anderson <james.r.andreson@utah.edu>
2017-04-19 10:57:01 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -20,9 +20,11 @@ if __name__ == '__main__':
     from setuptools import setup, find_packages
 
     packages = find_packages()
+    
+    #OK to use pools v1.3.1, no changes made for v1.3.2
 
     install_requires = ["nornir_pools>=1.3.1",
-                        "nornir_shared>=1.3.1",
+                        "nornir_shared>=1.3.2",
                         "numpy>=1.9.1",
                         "scipy>=0.13.2",
                         "matplotlib>=1.3.0",
@@ -30,7 +32,7 @@ if __name__ == '__main__':
                         "six"]
 
     dependency_links = ["git+https://github.com/nornir/nornir-pools#egg=nornir_pools-1.3.1",
-                        "git+https://github.com/nornir/nornir-shared#egg=nornir_shared-1.3.1"]
+                        "git+https://github.com/nornir/nornir-shared#egg=nornir_shared-1.3.2"]
 
     scripts = ['nornir-addtransforms = nornir_imageregistration.scripts.nornir_addtransforms:Execute',
                'nornir-assemble-tiles = nornir_imageregistration.scripts.nornir_assemble_tiles:Execute',
@@ -57,7 +59,7 @@ if __name__ == '__main__':
     setup(name='nornir_imageregistration',
           zip_safe=False,
           classifiers=classifiers,
-          version='1.3.1',
+          version='1.3.2',
           description="Contains the core image registration algorithms for aligning 2d images into larger mosaics and 3D volumes",
           author="James Anderson",
           author_email="James.R.Anderson@utah.edu",
