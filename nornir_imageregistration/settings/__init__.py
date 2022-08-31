d93ba38ab2761f06c8a1544ed0ada16af353cc59
James Anderson <james.r.anderson@utah.edu>
2022-08-31 15:47:06 -0700

    Added helper function to load/save translate settings files

--- nornir_imageregistration/settings/__init__.py
+++ nornir_imageregistration/settings/__init__.py
@@ -1,2 +1,19 @@
+import os
+import json
+
 from .grid_refinement import GridRefinement
 from .translate import TranslateSettings
+
+def GetOrSaveTranslateSettings(settings:TranslateSettings, path:str):
+    '''
+    Check if a .json file exists, if it does load and return it.  Otherwise 
+    save the provide settings file as a .json file 
+    '''
+    try:
+        with open(path, 'r') as jsonfile:
+            data = json.load(jsonfile)
+            return TranslateSettings(**data)
+    except:
+        with open(path, 'w') as jsonfile:
+            json_data = json.dump(settings.__dict__, jsonfile, sort_keys=True)
+        return settings
