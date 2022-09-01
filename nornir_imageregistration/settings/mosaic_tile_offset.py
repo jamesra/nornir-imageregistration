18e2a540851c86d3d4f7670fd39a45d508da6242
James Anderson <james.r.anderson@utah.edu>
2022-09-01 16:04:47 -0700

    Added ability to save/load manual tile offsets for use in translating mosaic tiles

---
+++ nornir_imageregistration/settings/mosaic_tile_offset.py
@@ -0,0 +1,137 @@
+'''
+Created on Aug 31, 2022
+
+@author: u0490822
+'''
+
+from collections import namedtuple
+import csv 
+import json
+
+#tile_offset = namedtuple("tile_offset",('A','B','X','Y', 'Comment'))
+class TileOffset(object):
+    
+    @property
+    def ID(self):
+        return (self._A, self._B)
+    
+    @property
+    def A(self):
+        return self._A
+    
+    @property
+    def B(self):
+        return self._B
+    
+    @property
+    def X(self):
+        return self._X
+    
+    @property
+    def Y(self):
+        return self._Y
+    
+    @property
+    def Comment(self):
+        return self._Comment
+    
+    @property
+    def Offset(self):
+        return (self._Y, self._X)
+    
+    def __init__(self, A,B,Y,X,Comment=None):
+        self._A = A if A < B else B
+        self._B = B if A < B else A
+        self._X = X
+        self._Y = Y
+        self._Comment = Comment
+        
+    def __str__(self):
+        return f"{self.A:4} {self.B:4} {self.Y:8.1f} {self.X:8.1f}{'' if self.Comment is None else ' ' + self.Comment}\n"
+    
+    def __eq__(self, other):
+        return self.A == other.A and self.B == other.B and self.X == other.X and self.Y == other.Y and self.Comment == other.Comment
+    
+    def __ne__(self, other):
+        return not self.__eq__(other.ID)
+    
+    def __ge__(self, other):
+        return self.ID.__ge__(other.ID)
+    
+    def __gt__(self, other):
+        return self.ID.__gt__(other.ID)
+    
+    def __le__(self, other):
+        return self.ID.__le__(other.ID)
+    
+    def __lt__(self, other):
+        return self.ID.__lt__(other.ID)
+
+def LoadMosaicOffsets(path:str):
+    
+    # with open(path, 'r') as f:
+    #     offsets = json.load(fp=f)
+    #     return offsets
+    #
+
+    offsets = []
+    
+    with open(path, 'r') as offsets_file:
+        csvReader = csv.reader(offsets_file, delimiter=' ', skipinitialspace=True, dialect=csv.Dialect.skipinitialspace) 
+        for (line_number, line) in enumerate(csvReader):
+            if len(line) == 0:
+                continue
+    
+            if line[0].startswith('#'):
+                continue
+    
+            #Skip the header without printing a parsing error
+            if line_number == 0 and line[0].startswith('A'):
+                continue
+    
+            try:
+                A = int(line[0])
+                B = int(line[1])
+    
+                if A > B:  #Swap the IDs so lowest ID is in A position
+                    t = A
+                    A = B
+                    B = t
+    
+                Y = float(line[2])
+                X = float(line[3])
+     
+                comment = None            
+                if len(line) >= 5: 
+                    if len(line[4]) > 0 and line[4] != "None":
+                        comment = ' '.join(line[4:]) 
+              
+                o = TileOffset(A=A,B=B,X=X,Y=Y,  Comment=comment)
+                offsets.append(o) 
+            except Exception as e:
+                print(f"Could not parse histogram line #{line_number}: {' '.join(line)}")
+                print(f"{e}")
+    
+    return offsets
+
+def SaveMosaicOffsets(offsets:list[TileOffset], path:str):
+    # with open(path,'w') as offsets_file:
+    #     #header = f"A B X Y Weight"
+    #     #offsets_file.write(header)
+    #     json.dump(obj=obj, fp=offsets_file, indent=2)
+    if offsets is None:
+        offsets = []
+        
+    sorted_input = sorted(offsets)
+    
+    with open(path, 'w') as offsets_file:
+        header = f"{'A':4} {'B':4} {'X':8} {'Y':8} Comment\n"
+        offsets_file.write(header)
+        for o in sorted_input:
+            A = o.A if o.A < o.B else o.B
+            B = o.B if o.A < o.B else o.A
+            output = f"{A:4} {B:4} {o.Y:8.1f} {o.X:8.1f} {'' if o.Comment is None else o.Comment}\n"
+            offsets_file.write(output) 
+
+if __name__ == '__main__':
+    pass
---
+++ test/settings/test_mosaicoffsets.py
@@ -0,0 +1,36 @@
+
+import unittest
+import os
+import nornir_imageregistration
+
+from test.setup_imagetest import TestBase
+
+
+class TestMosaicOffsetSettings(TestBase):
+    
+    def testSaveLoadMosaicOffsets(self):
+        
+        offsets = [nornir_imageregistration.settings.TileOffset(1,2,342.0,-23.9,None),
+                   nornir_imageregistration.settings.TileOffset(43,44,-231.2,34.1,"#Science should be funded"),
+                   nornir_imageregistration.settings.TileOffset(1,0,-131.2,54.2,"Yes"),
+                   nornir_imageregistration.settings.TileOffset(1,3,-1, 355.2),
+                   nornir_imageregistration.settings.TileOffset(998, 1001, -3848.4, 4352.000,"Yes")]
+        
+        offsets_path = os.path.join(self.TestOutputPath, "mosaic_offsets.json")
+        self.assertFalse(os.path.exists(offsets_path), f"{offsets_path} file should not exist at start of test")
+        
+        nornir_imageregistration.settings.SaveMosaicOffsets(offsets, offsets_path)
+        self.assertTrue(os.path.exists(offsets_path), f"{offsets_path} File should exist after saving")
+        
+        offsets_reload = nornir_imageregistration.settings.LoadMosaicOffsets(offsets_path)
+        self.assertTrue(len(offsets_reload) == len(offsets), "Settings loaded from disk should match value saved")
+        
+        sorted_offsets = sorted(offsets, key=lambda k: k.ID)
+        
+        for (i, _) in enumerate(sorted_offsets):
+            self.assertTrue(sorted_offsets[i] == offsets_reload[i], f'Item {i} does not match:\n\t{sorted_offsets[i]}\n\t{offsets_reload[i]}')
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testSaveLoadTranslateSettings']
+    unittest.main()
