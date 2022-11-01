Fixed issue with grid spacing being off by one pixel Fixed random noise not being added to stos refine regions when a continuous transform was being used. This fixed an issue where edges were registering poorly Added image stats to grid refinement settings to save recalculating them a lot Added missing type to boundingbox

nornir_imageregistration/core.py
@@ -123,7 +123,7 @@ def ApproxEqual(a, b, epsilon=None):
     return np.abs(a - b) < epsilon
 
 
-def ImageParamToImageArray(imageparam, dtype=None):
+def ImageParamToImageArray(imageparam: NDArray | str | memmap_metadata, dtype=None):
     image = None
     if isinstance(imageparam, np.ndarray):
         if dtype is None:
@@ -166,11 +166,11 @@ def ScalarForMaxDimension(max_dim, shapes):
     return max_dim / maxVal
 
 
-def ReduceImage(image, scalar):
+def ReduceImage(image: NDArray, scalar: float):
     return interpolation.zoom(image, scalar)
 
 
-def ExtractROI(image, center, area):
+def ExtractROI(image: NDArray, center, area):
     """Returns an ROI around a center point with the area, if the area passes a boundary the ROI
        maintains the same area, but is shifted so the entire area remains in the image.
        USES NUMPY (Y,X) INDEXING"""
@@ -267,7 +267,7 @@ def _ShrinkPillowImageFile(InFile: str, OutFile: str, Scalar: float, **kwargs):
 
 
 # Shrinks the passed image file, return procedure handle of invoked command
-def Shrink(InFile, OutFile, Scalar, **kwargs):
+def Shrink(InFile: str, OutFile: str, Scalar: float, **kwargs):
     """Shrinks the passed image file.  If Pool is not None the
        task is returned. kwargs are passed on to Pillow's image save function
        :param Scalar:
@@ -467,7 +467,7 @@ def CropImageRect(imageparam, bounding_rect, cval=None):
                      Height=int(bounding_rect.Height), cval=cval)
 
 
-def CropImage(imageparam:np.ndarray | str, Xo, Yo, Width:int, Height:int, cval: float | int = None):
+def CropImage(imageparam:np.ndarray | str, Xo, Yo, Width:int, Height:int, cval: float | int = None, image_stats: nornir_imageregistration.ImageStats | None = None):
     """
        Crop the image at the passed bounds and returns the cropped ndarray.
        If the requested area is outside the bounds of the array then the correct region is returned
@@ -565,7 +565,7 @@ def CropImage(imageparam:np.ndarray | str, Xo, Yo, Width:int, Height:int, cval: 
     cropped[out_startY:out_endY, out_startX:out_endX] = image[in_startY:in_endY, in_startX:in_endX]
 
     if not rMask is None:
-        return RandomNoiseMask(cropped, rMask, Copy=False)
+        return RandomNoiseMask(cropped, rMask, Copy=False, imagestats=image_stats)
 
     return cropped
 
@@ -1438,6 +1438,7 @@ def FindPeak(image, OverlapMask=None, Cutoff=None):
         PeakStrength = LabelSums[PeakValueIndex]
         #Because we offset the sum_labels call by 1, we must do the same for the PeakValueIndex
         PeakCenterOfMass = scipy.ndimage.measurements.center_of_mass(ThresholdImage, LabelImage, PeakValueIndex+1)
+        #PeakMaximumPosition = scipy.ndimage.maximum_position(ThresholdImage, LabelImage, PeakValueIndex+1)
         # nPixelsInLabel = np.sum(LabelImage == PeakValueIndex+1)
         # if (nPixelsInLabel / np.prod(image.shape)) > 0.001: #Tighten up the cutoff until the peak contains only about 1 in 1000 pixels in the threshold image
         #     new_cutoff = Cutoff + ((1.0 - Cutoff) / 2.0)
@@ -1452,15 +1453,16 @@ def FindPeak(image, OverlapMask=None, Cutoff=None):
             Weight = PeakStrength / FalsePeakStrength
          
         # print(f'{LabelSums.shape} Labels -> {PeakStrength} Peak')
+        
+        # center_of_mass returns results as (y,x)
+        # scaled_offset = (image.shape[0] / 2.0 - PeakCenterOfMass[0], image.shape[1] / 2.0 - PeakCenterOfMass[1])
+        scaled_offset = (np.asarray(image.shape, dtype=np.float32) / 2.0) - PeakCenterOfMass
+        # scaled_offset = (scaled_offset[0], scaled_offset[1])
 
         del LabelImage
         del ThresholdImage
         del LabelSums
 
-        # center_of_mass returns results as (y,x)
-        # scaled_offset = (image.shape[0] / 2.0 - PeakCenterOfMass[0], image.shape[1] / 2.0 - PeakCenterOfMass[1])
-        scaled_offset = (np.asarray(image.shape, dtype=np.float32) / 2.0) - PeakCenterOfMass
-        # scaled_offset = (scaled_offset[0], scaled_offset[1])
 
         return scaled_offset, Weight
 

nornir_imageregistration/grid_subdivision.py
@@ -37,7 +37,7 @@ class GridDivisionBase(object):
 
     def PopulateTargetPoints(self, transform):
         if transform is not None:
-            self.TargetPoints = np.round(transform.Transform(self.SourcePoints), 0).astype(np.int64)
+            self.TargetPoints = np.round(transform.Transform(self.SourcePoints), 3).astype(np.float)
             return self.TargetPoints
 
     def RemoveMaskedPoints(self, mask):
@@ -171,7 +171,7 @@ class ITKGridDivision(GridDivisionBase):
         elif grid_dims is None:
             self.grid_dims = nornir_imageregistration.TileGridShape(source_shape, grid_spacing)
 
-        self.grid_spacing = (source_shape - 1) / (self.grid_dims - 1)
+        self.grid_spacing = (source_shape) / (self.grid_dims - 1)
 
         self.coords = [np.asarray((iRow, iCol), dtype=np.int64) for iRow in range(self.grid_dims[0]) for iCol in
                        range(self.grid_dims[1])]

nornir_imageregistration/local_distortion_correction.py
@@ -282,7 +282,6 @@ r plots of each iteration in the output path for debugging purposes
                                                            grid_spacing=settings.grid_spacing)
     InputStos.Save(OutputStosPath)
 
-
 def RefineTransform(stosTransform,
                     settings:nornir_imageregistration.settings.GridRefinement,
                     SaveImages: bool=False,
@@ -298,8 +297,6 @@ def RefineTransform(stosTransform,
     :param stosTransform: The transform to refine
     """
 
-
-        
     if (SavePlots or SaveImages) and outputDir is None:
         raise ValueError("outputDir must be specified if SavePlots or SaveImages is true.")
     
@@ -549,6 +546,7 @@ def _RefinePointsForTwoImages(transform:nornir_imageregistration.transforms.ITra
     alignment_records = list()
      
     rigid_transforms = ApproximateRigidTransform(input_transform=transform, target_points=targetPoints)
+ 
      
     for i in range(nPoints):
          
@@ -558,8 +556,10 @@ def _RefinePointsForTwoImages(transform:nornir_imageregistration.transforms.ITra
                                            f"Align {key}",
                                            rigid_transforms[i],
                                            # Transform,
-                                           settings.target_image,
                                            settings.source_image,
+                                           settings.target_image,
+                                           settings.source_image_stats,
+                                           settings.target_image_stats,
                                            targetPoint,
                                            settings.cell_size,
                                            anglesToSearch=settings.angles_to_search,
@@ -828,7 +828,7 @@ an be offset before it is not eligible for finalization
     return finalize_mask
      
      
-def ApproximateRigidTransform(input_transform, target_points):
+def ApproximateRigidTransform(input_transform: nornir_imageregistration.ITransform, target_points: NDArray):
     """
     Given an array of points, returns a set of rigid transforms for each point that estimate the angle and offset for those two points to align.
     """
@@ -851,7 +851,7 @@ def ApproximateRigidTransform(input_transform, target_points):
     
     target_delta = offset_target_points - recalculated_target_points
     
-    angles = -nornir_imageregistration.ArcAngle(origins, offsets, target_delta)
+    angles = -np.round(nornir_imageregistration.ArcAngle(origins, offsets, target_delta), 3)
     
     target_offsets = target_points - source_points  
     
@@ -866,7 +866,10 @@ def ApproximateRigidTransform(input_transform, target_points):
 def StartAttemptAlignPoint(pool,
                            taskname: str,
                            transform: nornir_imageregistration.ITransform,
-                           targetImage: NDArray, sourceImage: NDArray,
+                           targetImage: NDArray, 
+                           sourceImage: NDArray,
+                           target_image_stats: nornir_imageregistration.ImageStats,
+                           source_image_stats: nornir_imageregistration.ImageStats,
                            controlpoint: NDArray | tuple[float, float],
                            alignmentArea: NDArray | tuple[float, float],
                            anglesToSearch: Iterable[float] | None = None,
@@ -885,12 +888,15 @@ def StartAttemptAlignPoint(pool,
     
     # Pull image subregions 
     sourceImageROI = nornir_imageregistration.assemble.WarpedImageToFixedSpace(transform,
-                            targetImage.shape, sourceImage, botleft=FixedRectangle.BottomLeft, area=FixedRectangle.Size, extrapolate=True)
+                            targetImage.shape, sourceImage, botleft=FixedRectangle.BottomLeft, area=FixedRectangle.Size, extrapolate=True, cval=np.nan)
+
+    sourceImageROI = nornir_imageregistration.RandomNoiseMask(sourceImageROI, np.logical_not(np.isnan(sourceImageROI)), imagestats=source_image_stats)
 
     targetImageROI = nornir_imageregistration.CropImage(targetImage,
                                                         FixedRectangle.BottomLeft[1], FixedRectangle.BottomLeft[0],
                                                         int(FixedRectangle.Size[1]), int(FixedRectangle.Size[0]),
-                                                        cval="random")
+                                                        cval="random",
+                                                        image_stats=target_image_stats)
 
     # Just ignore pure color regions
     if np.all(sourceImageROI == sourceImageROI[0][0]):