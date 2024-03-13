e5d65222126142711bed9680891a895ac99b318c
James Anderson <james.r.anderson@utah.edu>
2024-03-13 15:02:34 -0700

    Nearly working linear fit, but too slow

--- nornir_imageregistration/transforms/converters.py
+++ nornir_imageregistration/transforms/converters.py
@@ -1,12 +1,22 @@
 from typing import NamedTuple
-
+import enum
+from math import isclose
 import numpy as np
 from numpy.typing import NDArray
 
 import nornir_imageregistration
 from nornir_imageregistration.transforms import IControlPoints, ITransform, TransformType
 
+try:
+    import cupy as cp
+except ModuleNotFoundError:
+    import nornir_imageregistration.cupy_thunk as cp
+except ImportError:
+    import nornir_imageregistration.cupy_thunk as cp
+
 tau = np.pi * 2
+# _cross_product_abs_tolerance = 0.0001
+_cross_product_abs_tolerance = None
 
 
 class RigidComponents(NamedTuple):
@@ -15,6 +25,113 @@ class RigidComponents(NamedTuple):
     scale: float
     translation: NDArray[np.floating]
     reflected: bool
+
+
+class ControlPointRelation(enum.IntEnum):
+    LINEAR = 0  # Points are not flipped or colinear
+    FLIPPED = 1
+    COLINEAR = 2
+
+
+def _get_pointset_crossproducts(points: NDArray[np.floating]) -> float:
+    """
+    Returns the cross products of the vectors between the first point to other points in the array
+    """
+    global _cross_product_abs_tolerance
+    xp = cp.get_array_module(points)
+
+    if points.shape[0] < 3:
+        raise ValueError("Need at least 3 control points to determine if flipped")
+
+    # Calculate vectors
+    vectors = xp.diff(points, axis=0)  # Only need 2 vectors for speed
+
+    # Grid transforms in particular have may colinear points.  So we start our search for a non-zero cross product
+    # at the end of the list, and continue until we have two non-zero cross products
+    cross_products = xp.cross(vectors[0], vectors[1:])
+
+    return cross_products
+
+
+def _get_pointset_crossproduct(points: NDArray[np.floating]) -> float:
+    """
+    Returns the product of the vectors between the first and last point in the array
+    that creates a non-zero cross product if it exists.
+    """
+    cross_products = _get_pointset_crossproducts(points)
+
+    # Grid transforms in particular have may colinear points.  So we start our search for a non-zero cross product
+    # at the end of the list, and continue until we have two non-zero cross products
+    i_max_cross = np.argmax(abs(cross_products))
+
+    return cross_products[i_max_cross]
+
+
+def are_points_colinear(points: NDArray[np.floating]) -> ControlPointRelation:
+    """
+    Returns true if the control points are flipped on an axis. False otherwise.
+    Assume source_points and target_points are numpy arrays of shape (n, 2)
+    where n is the number of points and 2 corresponds to the x and y coordinates of each point
+    """
+    global _cross_product_abs_tolerance
+    cross = _get_pointset_crossproduct(points)
+    xp = cp.get_array_module(points)
+
+    # Check if flipped
+    # If either cross product is close to zero the points are colinear.
+    cross = 0 if isclose(cross, 0) else cross
+
+    # Both point sets are colinear
+    if cross == 0:
+        return ControlPointRelation.COLINEAR
+
+    return ControlPointRelation.LINEAR if xp.sign(cross) >= 0 else ControlPointRelation.FLIPPED
+
+
+def get_control_point_relationship(source_points, target_points) -> ControlPointRelation:
+    """
+    Returns true if the control points are flipped on an axis. False otherwise.
+    Assume source_points and target_points are numpy arrays of shape (n, 2)
+    where n is the number of points and 2 corresponds to the x and y coordinates of each point
+    """
+
+    xp = cp.get_array_module(source_points)
+
+    # TODO, make sure cross product is from the same pair of points in both sets, and they are the maximum cross product possible
+    source_crosses = _get_pointset_crossproducts(source_points)
+    target_crosses = _get_pointset_crossproducts(target_points)
+
+    source_crosses[np.isclose(source_crosses, 0)] = 0
+    target_crosses[np.isclose(target_crosses, 0)] = 0
+
+    non_zero_crosses = np.logical_and(source_crosses != 0, target_crosses != 0)
+
+    source_cross_signs = np.sign(source_crosses[non_zero_crosses])
+    target_cross_signs = np.sign(target_crosses[non_zero_crosses])
+
+    # Check how many cross products have matching signs
+    sign_comparisons = source_cross_signs == target_cross_signs
+    num_matching_signs = np.sum(sign_comparisons)
+    if num_matching_signs > len(sign_comparisons) / 2:
+        return ControlPointRelation.LINEAR
+
+    return ControlPointRelation.FLIPPED
+
+    # Check the signs of the cross products, and if more matches are found than not, return that result
+    sign_comparisons = np.sign(source_crosses) == np.sign(target_crosses)
+
+    if source_cross == ControlPointRelation.COLINEAR and target_cross == ControlPointRelation.COLINEAR:
+        return ControlPointRelation.COLINEAR
+
+    # If the cross product is zero the points are colinear, use the cross product of the non-colinear points
+    if source_cross == ControlPointRelation.COLINEAR:
+        return ControlPointRelation.LINEAR if xp.sign(target_cross) >= 0 else ControlPointRelation.FLIPPED
+
+    if target_cross == ControlPointRelation.COLINEAR:
+        return ControlPointRelation.LINEAR if xp.sign(source_cross) >= 0 else ControlPointRelation.FLIPPED
+
+    return ControlPointRelation.FLIPPED if xp.sign(source_cross) != xp.sign(
+        target_cross) else ControlPointRelation.LINEAR
 
 
 def _kabsch_umeyama(target_points: NDArray[np.floating], source_points: NDArray[np.floating]) -> tuple[
@@ -40,15 +157,22 @@ def _kabsch_umeyama(target_points: NDArray[np.floating], source_points: NDArray[
 
     H = (centered_A.T @ centered_B) / num_pts
     U, D, VT = np.linalg.svd(H)
-    # VT = VT.T
-    d = np.sign(np.linalg.det(VT.T @ U))
-    reflected = d < 0
+    # VT = VT.T # The determinate approach to flip detection was not working according to hypothesis testing
+    # d = np.sign(np.linalg.det(VT.T @ U))
+    # reflected = d < 0
+    relation = get_control_point_relationship(source_points, target_points)
+    reflected = relation == ControlPointRelation.FLIPPED
+    d = -1 if reflected else 1
     if reflected:
-        sp = source_points
+        sp = np.array(source_points)
         sp -= source_rotation_center  # Flip points across center
         sp[:, 0] = -sp[:, 0]
+        sp += source_rotation_center
         ignore_source_rotation_center, rotation_matrix, scale, translation, ignore_reflected = _kabsch_umeyama(
             target_points, sp)
+        if not np.allclose(ignore_source_rotation_center, source_rotation_center):
+            raise ArithmeticError("Flipping control points should not change the center of rotation")
+
         return source_rotation_center, rotation_matrix, scale, translation, True
 
     S = np.diag([1] * (num_dims - 1) + [d])
@@ -57,15 +181,10 @@ def _kabsch_umeyama(target_points: NDArray[np.floating], source_points: NDArray[
 
     rotation_matrix = U @ S @ VT
 
-    # OK, there are errors in the scale caluclation, to help it out I transform the points with the rotation, then re-run the algorithm with just scaling and translation
-    # sp = source_points if reflected == False else np.flipud(source_points)
     sp = source_points
-    source_points_B = (sp - source_rotation_center) @ rotation_matrix
 
-    scale, translation = _kabsch_umeyama_translation_scaling(target_points, source_points_B)
-    # translation = translation - source_rotation_center
+    scale = VarA / np.trace(np.diag(D) @ S)
 
-    #    scale = VarA / np.trace(np.diag(D) @ S)
     # Total translation, does not factor in translation of B to EB for rotation
     translation = EA - (scale * rotation_matrix @ EB)
 
@@ -78,8 +197,9 @@ def _kabsch_umeyama(target_points: NDArray[np.floating], source_points: NDArray[
     return source_rotation_center, rotation_matrix, scale, translation, reflected
 
 
-def _kabsch_umeyama_translation_scaling(target_points: NDArray[np.floating], source_points: NDArray[np.floating]) -> tuple[
-    NDArray[np.floating], float, NDArray[np.floating]]:
+def _kabsch_umeyama_translation_scaling(target_points: NDArray[np.floating], source_points: NDArray[np.floating]) -> \
+        tuple[
+            NDArray[np.floating], float, NDArray[np.floating]]:
     '''
     This function is used to get the translation and scaling factors when aligning
     points in B on reference points in A.
@@ -119,14 +239,14 @@ def EstimateRigidComponentsFromControlPoints(target_points: NDArray[np.floating]
 
         # My rigid transform is probably written in a weird way.  It translates source points to the center of rotation, translates them back, and then
         # performs the final translation into target space.
-        adjusted_translation = np.mean(target_points - source_points, axis=0)
+        adjusted_translation = np.mean(target_points - source_points, axis=0) + translation
         rotate_angle = np.arctan2(rotation_matrix[0, 1], rotation_matrix[0, 0])
         if rotate_angle <= -tau:
             rotate_angle += tau
         elif rotate_angle >= tau:
             rotate_angle -= tau
 
-        return RigidComponents(source_rotation_center=source_rotation_center, angle=rotate_angle,
+        return RigidComponents(source_rotation_center=np.zeros((1, 2), float), angle=rotate_angle,
                                translation=translation, scale=scale, reflected=reflected)
 
     else:
