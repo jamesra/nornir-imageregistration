83a51c097fc21103e3bd94a9d76e1b630380f545
James Anderson <james.r.anderson@utah.edu>
2024-11-18 12:22:03 -0800

    Move change events from my thread pool to the built in ThreadPoolExecutor

--- nornir_imageregistration/transforms/defaulttransformchangeevents.py
+++ nornir_imageregistration/transforms/defaulttransformchangeevents.py
@@ -1,10 +1,17 @@
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 from abc import ABCMeta
+from typing import List, Callable
 
 from nornir_imageregistration.transforms.base import ITransformChangeEvents
 import nornir_pools
 
 
 class DefaultTransformChangeEvents(ITransformChangeEvents, metaclass=ABCMeta):
+    # Static thread pool executor
+    _executor = ThreadPoolExecutor()
+    OnChangeEventListeners: List[Callable[[], None]]
+
     def __init__(self):
         self.OnChangeEventListeners = []
 
@@ -29,16 +36,22 @@ class DefaultTransformChangeEvents(ITransformChangeEvents, metaclass=ABCMeta):
         # Calls every listener when the transform has changed in a way that a point may be mapped to a new position in the fixed space
 
         if len(self.OnChangeEventListeners) > 1:
-            pool = nornir_pools.GetGlobalThreadPool()
-            tlist = list()
+            # with ThreadPoolExecutor as executor:
+            tlist = [DefaultTransformChangeEvents._executor.submit(func) for func in self.OnChangeEventListeners]
+            for task in concurrent.futures.as_completed(tlist):
+                # Call result to see exceptions
+                result = task.result()
 
-            for func in self.OnChangeEventListeners:
-                tlist.append(pool.add_task("OnTransformChanged calling " + str(func), func))
-
-            # Call wait on all tasks so we see exceptions
-            while len(tlist) > 0:
-                t = tlist.pop(0)
-                t.wait()
+            # pool = nornir_pools.GetGlobalThreadPool()
+            # tlist = list()
+            #
+            # for func in self.OnChangeEventListeners:
+            #     tlist.append(pool.add_task("OnTransformChanged calling " + str(func), func))
+            #
+            # # Call wait on all tasks so we see exceptions
+            # while len(tlist) > 0:
+            #     t = tlist.pop(0)
+            #     t.wait()
         else:
             for func in self.OnChangeEventListeners:
                 func()
