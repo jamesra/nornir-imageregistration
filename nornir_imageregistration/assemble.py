2f3656aeaf9047ac0a2aad38c86613f762d20d21
James Anderson <james.r.anderson@utah.edu>
2025-01-15 13:09:59 -0800

    Nearest caused pixels to be written past the bounds of the image.  grid-constant seems to work.

--- nornir_imageregistration/assemble.py
+++ nornir_imageregistration/assemble.py
@@ -338,7 +338,7 @@ def _TransformImageUsingCoords(target_coords: NDArray,
     try:
         outputValues = sp.ndimage.map_coordinates(subroi_warpedImage,
                                                   filtered_source_coords.transpose(),
-                                                  mode='nearest',
+                                                  mode='grid-constant',
                                                   order=order,
                                                   cval=cval,
                                                   prefilter=True).astype(original_dtype, copy=False)
