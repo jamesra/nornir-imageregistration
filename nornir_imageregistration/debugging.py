666a7d38eeb1f76f17fbbc18ff05fb736913de3a
James Anderson <james.r.anderson@utah.edu>
2025-05-28 16:37:11 -0700

    Fix a crash when debugger was attached but DEBUG environment variable was missing

--- nornir_imageregistration/debugging.py
+++ nornir_imageregistration/debugging.py
@@ -1,20 +1,25 @@
 import sys
 import os
 
+__in_debug_mode = False
+
 if 'DEBUG' in os.environ or sys.gettrace() is not None:
     try:
-        value = os.environ['DEBUG']
-        if value:
-            value = int(value)
-            __in_debug_mode = bool(value)
+        if 'DEBUG' in os.environ:
+            print('DEBUG environment variable is deprecated. Use DEBUG instead.')
+            value = os.environ['DEBUG']
+            if value:
+                value = int(value)
+                __in_debug_mode = bool(value)
+            else:
+                __in_debug_mode = True
         else:
             __in_debug_mode = True
     except ValueError:
         print('DEBUG environment variable must be 0 or 1 or empty.  (Empty is equivalent to 1)')
         raise
-else:
-    __in_debug_mode = False
 
 
 def in_debug_mode() -> bool:
+    global __in_debug_mode
     return __in_debug_mode
